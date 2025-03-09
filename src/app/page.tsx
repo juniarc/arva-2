19b16b5c780b11f33de1d517978417ad87fc7c5e
Cahya Juniar <juniarcahya@gmail.com>
2025-03-09 20:27:26 +0700

    test header desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import HomeDesktop from "@/containers/home-page/HomeDesktop";
-import HomeMobile from "@/containers/home-page/HomeMobile";
-import HomeWrapper from "@/containers/home-page/HomeWrapper";
-import { cookies } from "next/headers";
 
-export default async function Home() {
-  const cookiesList = await cookies();
-  const viewport = cookiesList.get("viewport")?.value || undefined;
-  return (
-    <HomeWrapper
-      viewport={viewport}
-      mobileHome={<HomeMobile />}
-      desktopHome={<HomeDesktop />}
-    />
-  );
+export default function Home() {
+  // const cookiesList = await cookies();
+  // const viewport = cookiesList.get("viewport")?.value || undefined;
+  // return (
+  //   <HomeWrapper
+  //     viewport={viewport}
+  //     mobileHome={<HomeMobile />}
+  //     desktopHome={<HomeDesktop />}
+  //   />
+  // );
+
+  return <HomeDesktop />;
 }
